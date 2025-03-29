ff18efa381c605b703c55d8cfb7226d923f48017
Haidukzz <seu-email@gmail.com>
2025-03-29 13:42:02 -0300

    Corrigido o uso da variável PORT para produção

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"text/template"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	// Obtenha a porta a partir da variável de ambiente 'PORT'
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Caso a variável de ambiente 'PORT' não esteja definida, use 8080 como padrão
+	}
+
+	// Inicia o servidor na porta determinada pela variável de ambiente 'PORT'
 	http.HandleFunc("/", index)
-	log.Fatal(http.ListenAndServe(":8080", nil)) // Log de erro se falhar ao iniciar o servidor
+	log.Fatal(http.ListenAndServe(":"+port, nil)) // Log de erro se falhar ao iniciar o servidor
 }
 
 func index(w http.ResponseWriter, r *http.Request) {

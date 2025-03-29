06f0e4fc2efe2b7cd527ecd20af9628d6714a502
Haidukzz <seu-email@gmail.com>
2025-03-29 13:48:18 -0300

    Adicionando logs de depuração

--- main.go
+++ main.go
@@ -10,20 +10,30 @@ import (
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	// Obtenha a porta a partir da variável de ambiente 'PORT'
+	// Log para verificar se o código está sendo executado até aqui
+	log.Println("Iniciando o servidor...")
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080" // Caso a variável de ambiente 'PORT' não esteja definida, use 8080 como padrão
 	}
 
-	// Inicia o servidor na porta determinada pela variável de ambiente 'PORT'
+	// Log para mostrar qual porta será utilizada
+	log.Printf("Escutando na porta: %s\n", port)
+
+	// Configura a rota e inicia o servidor
 	http.HandleFunc("/", index)
-	log.Fatal(http.ListenAndServe(":"+port, nil)) // Log de erro se falhar ao iniciar o servidor
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %s", err) // Log de erro se falhar ao iniciar
+	}
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	log.Println("Acessando a rota principal...")
 	err := temp.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
+		log.Printf("Erro ao renderizar template: %s\n", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
